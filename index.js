9be7752847de84aba48cdfb6f91774e682c25bfe
LullabyOfLies <jeanlorenz.recato@tup.edu.ph>
2023-03-08 22:11:34 +0800

    timestamp added| print orientation must be column

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
-var d = new Date();
-var n = d.toLocaleTimeString();
 
 function increment() {
     count += 1
@@ -10,8 +8,10 @@ function increment() {
 }
 
 function save() {
-    let countStr = "\n" + n + ": " + count + " people\n"
-    saveEl.textContent += countStr
+    let now = new Date();
+    let timeStr = now.toLocaleTimeString();
+    let countStr = "\n" + timeStr + " : " + count + " people\n"
+    saveEl.textContent += "\n" + countStr
     countEl.textContent = 0
     count = 0
 }
